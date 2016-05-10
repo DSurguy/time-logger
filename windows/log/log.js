9f0015082de92550fde43ea6ee9fe744f4f4be80
Derek Surguy <d.surguy@austinlane.com>
2016-05-10 16:35:00 -0500

    Displaying records from datastore on log

--- windows/log/log.js
+++ windows/log/log.js
@@ -16,12 +16,25 @@ function setupDatePicker(){
 }
 
 function updateDateSelection(mDate){
-	document.querySelector('.date-selector').value = mDate.format('MM / DD / YYYY')
+	document.querySelector('.date-selector').value = mDate.format('MM / DD / YYYY');
 	loadRecordList(mDate);
 }
 
 function loadRecordList(mDate){
-
+	Database.store('records').find({
+		date: mDate.format('YYYYMMDD')
+	}).sort({time: 1}).exec( (err, docs) => {
+		//grab and empty the table body
+		var recordTable = document.querySelector('.record-list tbody');
+		recordTable.innerHTML = '';
+		//add all the records
+		for( var i=0; i<docs.length; i++ ){
+			var newRow = document.createElement('tr');
+			newRow.innerHTML = '<td>'+docs[i].text+'</td>'
+				+'<td>'+docs[i].time+'</td>';
+			recordTable.appendChild(newRow);
+		}
+	});
 }
 
 window.onload = function (){
