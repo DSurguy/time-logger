9570fa383d063b6c52c5074cc80486ea95607d70
Derek Surguy <surguy.derek@gmail.com>
2016-08-03 20:08:08 -0500

    Implemented diffing log times

--- windows/log/log.js
+++ windows/log/log.js
@@ -30,13 +30,63 @@ function loadRecordList(mDate){
 		//add all the records
 		for( var i=0; i<docs.length; i++ ){
 			var newRow = document.createElement('tr');
-			newRow.innerHTML = '<td>'+docs[i].text+'</td>'
+			let htmlString = '<td>'+docs[i].text+'</td>'
 				+'<td>'+docs[i].time+'</td>';
+			if( i > 0 ){
+				htmlString +='<td>'+diffTime(docs[i].time,docs[i-1].time)+'</td>'; 
+			}
+			else{
+				htmlString +='<td></td>';
+			}
+			newRow.innerHTML = htmlString; 
 			recordTable.appendChild(newRow);
 		}
 	});
 }
 
+//time format should me hh:mm:ss
+function diffTime(time1, time2){
+	let secondsDiff;
+	if( parseInt(time1.replace(/:/g,'')) > parseInt(time2.replace(/:/g,'')) ){
+		secondsDiff = convertTimeToSeconds(time1) - convertTimeToSeconds(time2);
+	}
+	else{
+		secondsDiff = convertTimeToSeconds(time2) - convertTimeToSeconds(time1);
+	}
+	//convert the times to seconds
+	return convertSecondsToTime(secondsDiff);
+}
+
+function convertTimeToSeconds(timeString){
+	return timeString.split(/:/g).reduce((total, cur, index) => {
+		switch (index){
+			case 0: total += parseInt(cur)*60*60; break;
+			case 1: total += parseInt(cur)*60; break;
+			case 2: total += parseInt(cur);
+		}
+		return total;
+	},0);
+}
+
+function convertSecondsToTime(seconds){
+	let hours = parseInt(seconds/60/60);
+	seconds = seconds - hours*60*60;
+	let mins = parseInt(seconds/60);
+	seconds = seconds - mins*60;
+	return leftPadZero(hours)+':'+leftPadZero(mins)+':'+leftPadZero(seconds);
+}
+
+function leftPadZero(val, length){
+	let valLength = (val+'').length; 
+	if( valLength < length ){
+		let paddedString = '';
+		for( let i=valLength; i<valLength;i++ ){
+			paddedString += '0';
+		}
+		return paddedString + val+'';
+	}
+	return val+'';
+}
 window.onload = function (){
 	Database.init().then( () => {
 		setupDatePicker();
